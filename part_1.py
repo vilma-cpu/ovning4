62c518270dc3d18c99139a53c896924eedcb9140
vilma-cpu <vilma.goding.rask@gmail.com>
2026-02-03 14:48:06 +0000

    Part 1 done

--- part_1.py
+++ part_1.py
@@ -1,3 +1,10 @@
 #   create_file_from_string(my_filename, my_string)
 #        som skapar en fil med namnet my_filename (en sträng), och fyller den med innehållet i  my_string (en sträng)
 #        Exempel på användning: create_file_from_string("min_superfil.txt", "hurra för mig")
+
+def create_file_from_string(my_filename, my_string):
+    with open(my_filename, "w") as file:
+        file.write(my_string)
+
+
+create_file_from_string("min_superfil.txt", "hurra för mig")
