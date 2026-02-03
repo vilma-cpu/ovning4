90a805b8e9caa2ce639dcee669cac324465a56e0
vilma-cpu <vilma.goding.rask@gmail.com>
2026-02-03 14:51:56 +0000

    Part 2 done without AI

--- part_2.py
+++ part_2.py
@@ -1,3 +1,10 @@
 #    print_file_on_screen(my_filename)
 #        som öppnar filen med namnet my_filename (en sträng) och skriver ut innehållet på skärmen
 #        Exempel på användning: print_file_on_screen("min_superfil.txt")
+
+def print_file_on_screen(my_filename):
+    with open(my_filename, "r") as file:
+        content = file.read()
+        print(content)
+
+print_file_on_screen("min_superfil.txt")
